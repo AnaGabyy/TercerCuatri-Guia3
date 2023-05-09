b9cce21ace1d51d3c5cbf7f8310003c1decf2b11
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 20:08:54 -0300

    Interface MetodosLista

---
+++ TercerCuatri-Guia3/src/ejercicioUno/MetodosLista.java
@@ -0,0 +1,11 @@
+package ejercicioUno;
+
+import java.util.List;
+
+public interface MetodosLista {
+
+    public void agregarTarea(ListaTareas.Tarea tarea);
+    public void eliminarTarea(ListaTareas.Tarea tarea);
+    public String cantidadTareas();
+    public void obtenerTareas();
+}
