a852106bb70608d7cc4d70179d44fef305450f53
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 20:15:43 -0300

    Clase Main ej3
    Enunciado

---
+++ TercerCuatri-Guia3/src/ejercicioTresTest/Main.java
@@ -0,0 +1,13 @@
+package ejercicioTresTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Se desea modelar un zoológico que contenga una lista de animales. Los animales son representados mediante
+        una Clase Animal que define un método getName() para obtener el nombre del animal. Se desea implementar
+        el patrón singleton para garantizar que solo haya una instancia del zoológico. Además, se desea que la clase
+        Zoo tenga una clase interna llamada ZooKeeper que pueda alimentar a los animales en el zoológico. Se requiere
+        utilizar únicamente la colección ArrayList para almacenar los animales en el zoológico.*/
+
+    }
+}
