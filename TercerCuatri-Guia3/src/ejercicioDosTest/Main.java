f746b7ae8e015162bfeb5124acecfb8cd0a0a33f
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 20:15:28 -0300

    Clase Main ej2
    Enunciado

---
+++ TercerCuatri-Guia3/src/ejercicioDosTest/Main.java
@@ -0,0 +1,11 @@
+package ejercicioDosTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Implementación de una clase llamada supermercado que contiene un catálogo de productos que utiliza el patrón
+        Singleton para garantizar que solo se cree una instancia del supermercado en tode el programa, una clase
+        interna para almacenar los productos y una interfaz para definir los métodos del catálogo*/
+
+    }
+}
